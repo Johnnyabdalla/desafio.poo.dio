fb14d69ca7066faef3b2ad85efc45f36ef1a3d69
Joao Abdalla <joaoabdalla27@gmail.com>
2023-04-29 23:05:09 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
+import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello  opa and welcome!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
 
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição Curso JavaScript");
+        curso2.setCargaHoraria(4);
 
-        for (int i = 1; i <= 5; i++) {
+        Mentoria mentoria1= new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição Mentoria Java");
+        mentoria1.setData(LocalDate.now());
 
-            System.out.println("i = " + i);
-        }
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
     }
 }
